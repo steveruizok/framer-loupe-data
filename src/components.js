d210d74061e4d44ff8fccddc824602a396281ade
Steve Ruiz <steveruizok@gmail.com>
2019-08-05 15:58:46 +0100

    Adds content

--- src/components.js
+++ src/components.js
@@ -1,7 +1,11 @@
-import { GoMarkGithub } from 'react-icons/go'
-import { FaTwitter } from 'react-icons/fa'
+import { GoMarkGithub } from "react-icons/go";
+import { FaTwitter } from "react-icons/fa";
+import { IoIosBasketball } from "react-icons/io";
+import Instructor from "./components/instructor";
 
 export default {
+	Instructor,
+	SunnyIcon: IoIosBasketball,
 	GithubIcon: GoMarkGithub,
-	TwitterIcon: FaTwitter,
-}
+	TwitterIcon: FaTwitter
+};
