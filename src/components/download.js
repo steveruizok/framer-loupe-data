1d16e4ffe6fab83b8d3d62eaf2ef1fb625a91e9a
Steve Ruiz <steveruizok@gmail.com>
2019-08-09 13:50:24 +0100

    Fixes download

--- src/components/download.js
+++ src/components/download.js
@@ -4,36 +4,42 @@ import { MdFileDownload } from "react-icons/md";
 
 function Download({ title = "Project.framerx", url = "" }) {
 	return (
-		<a
-			target="_blank"
-			rel="noopener noreferrer"
-			href={url}
-			sx={{ textDecoration: "none" }}
+		<div
+			sx={{
+				width: ["auto", "fit-content"],
+				py: 3,
+				px: 4,
+				mb: 4,
+				borderRadius: 8,
+				bg: "primary",
+				color: "#FFF",
+				h3: {
+					marginTop: 0,
+					marginBottom: 0,
+					lineHeight: 1
+				},
+				a: {
+					color: "#FFF",
+					textDecoration: "none"
+				}
+			}}
 		>
-			<div
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href={url}
 				sx={{
-					width: ["auto", "fit-content"],
+					textDecoration: "none",
 					display: "flex",
 					flexDirection: "row",
 					placeItems: "center",
-					placeContent: "center",
-					py: 3,
-					px: 4,
-					mb: 4,
-					borderRadius: 8,
-					bg: "primary",
-					color: "muted",
-					h3: {
-						marginTop: 0,
-						marginBottom: 0,
-						lineHeight: 1
-					}
+					placeContent: "center"
 				}}
 			>
 				<MdFileDownload size="1.5em" />
-				<h3 sx={{ ml: 2 }}>{title}</h3>
-			</div>
-		</a>
+				<h3 sx={{ ml: 2, display: "inline-block" }}>{title}</h3>
+			</a>
+		</div>
 	);
 }
 
